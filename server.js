35eda2911e9643e0efb33e4b6db5671d4311c7eb
ghsiqueira <gabrielh2805@gmail.com>
2025-06-18 11:20:54 -0300

    update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(limiter);
 
 // CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3001',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
 
